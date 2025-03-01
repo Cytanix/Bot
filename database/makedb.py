bc2ce290cd6be1d2165970f0a204086af1d7b6f4
SpiritTheWalf <spirit@tachyonind.org>
2025-03-01 16:01:11 +0000

    Add copyright

--- database/makedb.py
+++ database/makedb.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 SpiritTheWalf and Cytanix
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+# To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/ or see the LICENSE file.
 """This file contains the database logic, including the tables and connection data"""
 import os
 import asyncio
