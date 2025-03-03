a6386fdac44680924098205e2a7b0e8add1d177a
SpiritTheWalf <spirit@tachyonind.org>
2025-03-03 23:13:15 +0000

    Add copyright

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 SpiritTheWalf and Cytanix
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+# To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/ or see the LICENSE file.
+"""Contains owner only commands"""
 import asyncio
 import os
 import sys
--- utils/logger.py
+++ utils/logger.py
@@ -1,4 +1,8 @@
-"""Custom Logger for Ransas"""
+"""Custom Logger for Cytanix"""
+# Copyright (c) 2025 SpiritTheWalf and Cytanix
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+# To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/ or see the LICENSE file.
 import os
 import logging
 import sys
