c98ab7c7d9c9291fc637a39d1e417dcf808013d9
SpiritTheWalf <spiritthewalf@gmail.com>
2025-03-14 14:02:13 +0000

    Refactor and optimise Logs class

--- database/db_io.py
+++ database/db_io.py
@@ -3,102 +3,102 @@
 # This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 # To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/ or see the LICENSE file.
 """Contains the functions to interact with the database"""
-from typing import Union
-import sqlalchemy
+from typing import Union, Any
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import select
-from .makedb import session_factory, Logs as DbLog, Punishments as DbPunishments
+from sqlalchemy.orm import Session
+from utils.logger import logger
+from .makedb import session_factory, Logs as DbLog, Punishments as DbPunishments, CustomCommands as DbCc
 
 class Logs:
     """Defines the log structure"""
 
     @staticmethod
+    async def _get_guild_entry(guild_id: int, session: Session) -> Union[DbLog, None]:
+        """Helper method to get a guild entry"""
+        return (await session.execute(select(DbLog).where(DbLog.guild_id == guild_id))).scalars().first()
+
+
+    @staticmethod
     async def check_guild(guild_id: int) -> bool:
         """Checks if the guild is already in the database"""
         async with session_factory() as session:
-            result = (await session.execute(select(DbLog).filter(DbLog.guild_id == guild_id))).scalars().first()
-            if result:
-                return True
-            return False
+            return await Logs._get_guild_entry(guild_id, session) is not None
 
-    @staticmethod
-    async def add_guild_entry( # pylint: disable=R0913,R0917
-            guild_id: str,
-            message_logging_channel_id: str,
-            member_logging_channel_id: str,
-            voice_logging_channel_id: str,
-            moderation_logging_channel_id: str,
-            muterole_role_id: str,
-            muterole_logging_channel_id: str,
-            reaction_logging: str,) -> str:
-        """Add a guild to the log table"""
-        async with session_factory() as session:
-            guild_entry = (await session.execute(select(DbLog).filter(DbLog.guild_id == guild_id))).scalars().first()
-            if guild_entry:
-                try:
-                    setattr(guild_entry, "message_logging_channel_id", message_logging_channel_id)
-                    setattr(guild_entry, "member_logging_channel_id", member_logging_channel_id)
-                    setattr(guild_entry, "voice_logging_channel_id", voice_logging_channel_id)
-                    setattr(guild_entry, "moderation_logging_channel_id", moderation_logging_channel_id)
-                    setattr(guild_entry, "muterole_logging_channel_id", muterole_logging_channel_id)
-                    setattr(guild_entry, "muterole_role_id", muterole_role_id)
-                    setattr(guild_entry, "reaction_logging", reaction_logging)
-                    await session.commit()
-                    return "The operation completed successfully."
-                except sqlalchemy.exc.SQLAlchemyError as e:
-                    await session.rollback()
-                    print(e)
-                    return "Something went wrong, please check error logs."
-            else:
-                return "The guild does not exist."
 
     @staticmethod
     async def get_guild(guild_id: int) -> Union[DbLog, None, str]:
         """Gets a guild from the database"""
         async with session_factory() as session:
             try:
-                entry: Union[DbLog, None] = (await session.execute(select(DbLog).filter(DbLog.guild_id == guild_id))).scalars().first()
-                if entry:
-                    return entry
-                return None
-
-            except sqlalchemy.exc.SQLAlchemyError as e:
-                await session.rollback()
-                print(e)
+                return await Logs._get_guild_entry(guild_id, session)
+            except SQLAlchemyError as e:
+                logger.error("Database error while getting guild entry: %s", e, exc_info=True)
                 return "Something went wrong, please check error logs."
+
 
     @staticmethod
     async def remove_guild(guild_id: int)-> str:
         """Removes a guild from the database"""
         async with session_factory() as session:
             try:
-                entry = (await session.execute(select(DbLog).filter(DbLog.guild_id == guild_id))).scalars().first()
+                entry = await Logs._get_guild_entry(guild_id, session)
+                if entry is None:
+                    return f"Guild with id {guild_id} does not exist."
                 await session.delete(entry)
                 await session.commit()
                 return f"Guild with id {guild_id} was removed."
 
-            except sqlalchemy.exc.SQLAlchemyError as e:
-                await session.rollback()
-                print(e)
+            except SQLAlchemyError as e:
+                if session.in_transaction():
+                    await session.rollback()
+                logger.error("Database error while removing guild entry: %s\nRolling back...", e, exc_info=True)
                 return "Something went wrong, please check error logs."
+
 
     @staticmethod
     async def add_guild_on_join(guild_id: int) -> str:
         """Adds a guild to the log table"""
-        try:
-            async with session_factory() as session:
-                guild_entry = DbLog()
-                setattr(guild_entry, "guild_id", guild_id)
+        async with session_factory() as session:
+            try:
+                guild_entry = DbLog(guild_id=guild_id)
                 await session.add(guild_entry)
                 await session.commit()
                 return f"Guild with id {guild_id} added to the database successfully."
 
-        except sqlalchemy.exc.SQLAlchemyError as e:
-            await session.rollback()
-            print(e)
-            return "Something went wrong, please check error logs."
+            except SQLAlchemyError as e:
+                if session.in_transaction():
+                    await session.rollback()
+                logger.error("Database error while adding guild entry on guild join: %s\nRolling back...", e, exc_info=True)
+                return "Something went wrong, please check error logs."
+
+
+    @staticmethod
+    async def update_guild(guild_id: int, **kwargs: Any) -> str:
+        """Updates a guild's log entry"""
+        async with session_factory() as session:
+            guild_entry = await Logs._get_guild_entry(guild_id, session)
+            if guild_entry:
+                try:
+                    valid_fields = {c.name for c in DbLog.__table__.columns}
+                    updates = {k: v for k, v in kwargs.items() if k in valid_fields and v is not None}
+                    for key, value in updates.items():
+                        setattr(guild_entry, key, value)
+
+                    await session.commit()
+                    return "The operation completed successfully."
+                except SQLAlchemyError as e:
+                    if session.in_transaction():
+                        await session.rollback()
+                    logger.error("Database error while updating guild entry: %s\nRolling back...", e, exc_info=True)
+                    return "Something went wrong, please check error logs."
+            else:
+                return "The guild does not exist."
+
 
 class Punishments:
     """Defines the punishment structure"""
+
 
     @staticmethod
     async def add_punishment( # pylint: disable=R0913,R0917
@@ -122,10 +122,11 @@ class Punishments:
                 await session.commit()
                 return "The punishment was added to the database successfully."
 
-        except sqlalchemy.exc.SQLAlchemyError as e:
+        except SQLAlchemyError as e:
             await session.rollback()
-            print(e)
+            logger.error("Database error while adding punishment entry: %s", e, exc_info=True)
             return "Something went wrong, please check error logs."
+
 
     @staticmethod
     async def get_punishment(punishment_id: int, guild_id: int, user_id: int) -> Union[DbPunishments, None, str]:
@@ -139,10 +140,11 @@ class Punishments:
                 if entry:
                     return entry
                 return None
-            except sqlalchemy.exc.SQLAlchemyError as e:
+            except SQLAlchemyError as e:
                 await session.rollback()
                 print(e)
                 return "Something went wrong, please check error logs."
+
 
     @staticmethod
     async def delete_punishment(punishment_id: int) -> str:
@@ -157,7 +159,33 @@ class Punishments:
 
                 return "There was an error deleting the punishment."
 
-            except sqlalchemy.exc.SQLAlchemyError as e:
+            except SQLAlchemyError as e:
                 await session.rollback()
                 print(e)
                 return "Something went wrong, please check error logs."
+
+
+class CustomCommands: # pylint: disable=R0903
+    """Defines the custom commands structure"""
+
+
+    @staticmethod
+    async def add_custom_command(name: str, owner_id: str, text: str, image: bytes, nsfw: bool) -> str:
+        """Adds a custom command to the database"""
+        try:
+            async with session_factory() as session:
+                custom_command_entry = DbCc()
+                setattr(custom_command_entry, "name", name)
+                setattr(custom_command_entry, "owner_id", owner_id)
+                setattr(custom_command_entry, "text", text)
+                setattr(custom_command_entry, "image", image)
+                setattr(custom_command_entry, "nsfw", nsfw)
+                await session.add(custom_command_entry)
+                await session.commit()
+                return "The custom command was added to the database successfully."
+
+        except SQLAlchemyError as e:
+            if session.in_transaction():
+                await session.rollback()
+            logger.error("Database error while adding custom command: %s\nRolling back....", e, exc_info=e)
+            return "Something went wrong, please check error logs."
