598319be2dec4bdb89c35ce78dd41000e2a0cc64
SpiritTheWalf <spiritthewalf@gmail.com>
2025-03-14 16:51:25 +0000

    Refactor and optimise Punishment class, removed CustomCommands temporarily so I can commit

--- database/db_io.py
+++ database/db_io.py
@@ -8,7 +8,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from utils.logger import logger
-from .makedb import session_factory, Logs as DbLog, Punishments as DbPunishments, CustomCommands as DbCc
+from .makedb import session_factory, Logs as DbLog, Punishments as DbPunishments
 
 class Logs:
     """Defines the log structure"""
@@ -16,7 +16,7 @@ class Logs:
     @staticmethod
     async def _get_guild_entry(guild_id: int, session: Session) -> Union[DbLog, None]:
         """Helper method to get a guild entry"""
-        return (await session.execute(select(DbLog).where(DbLog.guild_id == guild_id))).scalars().first()
+        return (await session.execute(select(DbLog).filter(DbLog.guild_id == guild_id))).scalars().first()
 
 
     @staticmethod
@@ -99,60 +99,58 @@ class Logs:
 class Punishments:
     """Defines the punishment structure"""
 
+    @staticmethod
+    async def _get_punishment_entry(punishment_id: int, session: Session) -> Union[DbPunishments, None]:
+        """Helper method to get a punishment entry"""
+        return (await session.execute(DbPunishments).filter(DbPunishments.punishment_id == punishment_id)).scalars().first()
+
 
     @staticmethod
-    async def add_punishment( # pylint: disable=R0913,R0917
-            guild_id: int,
-            user_id: int,
-            punishment: str,
-            punishment_time: int,
-            moderator_id: int,
-            reason: str,) -> str:
+    async def add_punishment(**kwargs: Any) -> str:
         """Adds a punishment to the database"""
-        try:
-            async with session_factory() as session:
+        async with session_factory() as session:
+            try:
                 punishment_entry = DbPunishments()
-                setattr(punishment_entry, "guild_id", guild_id)
-                setattr(punishment_entry, "user_id", user_id)
-                setattr(punishment_entry, "punishment", punishment)
-                setattr(punishment_entry, "punishment_time", punishment_time)
-                setattr(punishment_entry, "moderator_id", moderator_id)
-                setattr(punishment_entry, "reason", reason)
+                valid_fields = {c.name for c in DbPunishments.__table__.columns}
+                updates = {k: v for k, v in kwargs.items() if k in valid_fields and v is not None}
+                for key, value in updates.items():
+                    setattr(punishment_entry, key, value)
                 await session.add(punishment_entry)
                 await session.commit()
                 return "The punishment was added to the database successfully."
 
-        except SQLAlchemyError as e:
-            await session.rollback()
-            logger.error("Database error while adding punishment entry: %s", e, exc_info=True)
-            return "Something went wrong, please check error logs."
-
-
-    @staticmethod
-    async def get_punishment(punishment_id: int, guild_id: int, user_id: int) -> Union[DbPunishments, None, str]:
-        """Gets a punishment from the database"""
-        async with session_factory() as session:
-            try:
-                entry: Union[DbPunishments, None] = (await session.execute(select(DbPunishments).filter(
-                    DbPunishments.punishment_id == punishment_id,
-                    DbPunishments.guild_id == guild_id,
-                    DbPunishments.user_id == user_id))).scalars().first()
-                if entry:
-                    return entry
-                return None
             except SQLAlchemyError as e:
                 await session.rollback()
-                print(e)
+                logger.error("Database error while adding punishment entry: %s", e, exc_info=True)
                 return "Something went wrong, please check error logs."
 
 
     @staticmethod
-    async def delete_punishment(punishment_id: int) -> str:
+    async def get_punishment(punishment_id: int, guild_id: int) -> Union[DbPunishments, str]:
+        """Gets a punishment from the database"""
+        async with session_factory() as session:
+            try:
+                entry = await Punishments._get_punishment_entry(punishment_id, session)
+                if entry:
+                    if entry.guild_id == guild_id:
+                        return entry
+                    return ("Unable to find entry. Are you sure you have the correct ID.\n"
+                            "-# Please note, punishments can only be viewed in the guild they came from.")
+                return "Punishment not found."
+            except SQLAlchemyError as e:
+                logger.error("Database error while getting punishment entry: %s", e, exc_info=True)
+                return "Something went wrong, please check error logs."
+
+
+    @staticmethod
+    async def delete_punishment(punishment_id: int, guild_id: int) -> str:
         """Deletes a punishment from the database"""
         async with session_factory() as session:
             try:
-                entry = (await session.execute(select(DbPunishments).filter(DbPunishments.punishment_id == punishment_id))).scalars().first()
-                if entry:
+                entry = await Punishments._get_punishment_entry(punishment_id, session)
+                if entry is None:
+                    return "Punishment not found."
+                if entry.guild_id == guild_id:
                     await session.delete(entry)
                     await session.commit()
                     return "The punishment was deleted successfully."
@@ -161,31 +159,5 @@ class Punishments:
 
             except SQLAlchemyError as e:
                 await session.rollback()
-                print(e)
+                logger.error("Database error while deleting punishment: %s\nRolling back...", e, exc_info=True)
                 return "Something went wrong, please check error logs."
-
-
-class CustomCommands: # pylint: disable=R0903
-    """Defines the custom commands structure"""
-
-
-    @staticmethod
-    async def add_custom_command(name: str, owner_id: str, text: str, image: bytes, nsfw: bool) -> str:
-        """Adds a custom command to the database"""
-        try:
-            async with session_factory() as session:
-                custom_command_entry = DbCc()
-                setattr(custom_command_entry, "name", name)
-                setattr(custom_command_entry, "owner_id", owner_id)
-                setattr(custom_command_entry, "text", text)
-                setattr(custom_command_entry, "image", image)
-                setattr(custom_command_entry, "nsfw", nsfw)
-                await session.add(custom_command_entry)
-                await session.commit()
-                return "The custom command was added to the database successfully."
-
-        except SQLAlchemyError as e:
-            if session.in_transaction():
-                await session.rollback()
-            logger.error("Database error while adding custom command: %s\nRolling back....", e, exc_info=e)
-            return "Something went wrong, please check error logs."
