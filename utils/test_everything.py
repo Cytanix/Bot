b53fc32c7d4825d7617842c3db81d6daeac9f97d
SpiritTheWalf <spiritthewalf@gmail.com>
2025-04-03 13:28:27 +0100

    Add script to run all tests at once

---
+++ utils/test_everything.py
@@ -0,0 +1,22 @@
+"""Runs every test"""
+import subprocess
+import sys
+import os
+
+python_files = [
+    'test_connection.py',
+    'test_levels.py',
+    'test_logs.py',
+    'test_punishments.py',
+    'test_registration.py'
+]
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+for file in python_files:
+    print(f"Running {file}")
+    subprocess.run(['.venv\\Scripts\\python.exe', '-m', 'utils.' + file.replace("\\", ".").replace(".py", "")],
+                   stdout=sys.stdout,
+                   stderr=sys.stderr,
+                   env={**os.environ, 'PYTHONPATH': project_root},
+                   check=True)
